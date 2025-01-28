21a67eb1de07c4a6cd50680660bf8eb970070381
GarimaUttam <garimauttam191203@gmail.com>
2025-01-28 06:32:03 +0530

    update and delete product

--- src/main/java/com/garima/ecom_project/service/ProductService.java
+++ src/main/java/com/garima/ecom_project/service/ProductService.java
@@ -32,8 +32,18 @@ public class ProductService {
         product.setImageType(imageFile.getContentType());
         product.setImageData(imageFile.getBytes());
 
+        return repo.save(product);
+    }
 
+    public Product updateProduct(int id, Product product, MultipartFile imageFile) throws IOException {
+        product.setImageData(imageFile.getBytes());
+        product.setImageName(imageFile.getOriginalFilename());
+        product.setImageType(imageFile.getContentType());
         return repo.save(product);
 
+    }
+
+    public void deleteProduct(int id) {
+        repo.deleteById(id);
     }
 }
